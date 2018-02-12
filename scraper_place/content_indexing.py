31406c66a270415a7db32d722db4de51a2c0d4a1
Michel Blancard <github@michelblancard.fr>
2018-02-12 16:14:38 +0100

    Use EC2 for tika

--- scraper_place/content_indexing.py
+++ scraper_place/content_indexing.py
@@ -12,8 +12,10 @@ import urllib
 
 import psycopg2
 import requests
+import boto3
+import paramiko
 
-from scraper_place.config import CONFIG_TIKA, CONFIG_DATABASE, CONFIG_ELASTICSEARCH, CONFIG_ENV, STATE_GLACIER_OK, STATE_CONTENT_INDEXATION_OK, build_internal_filepath
+from scraper_place.config import CONFIG_DATABASE, CONFIG_ELASTICSEARCH, CONFIG_AWS_EC2, CONFIG_ENV, STATE_GLACIER_OK, STATE_CONTENT_INDEXATION_OK, build_internal_filepath
 
 
 def index():
@@ -37,17 +39,22 @@ def index():
         ;""",
         (STATE_GLACIER_OK, )
     )
-
     dce_data_list = cursor.fetchall()
+
+    ec2_client, instance_id, ec2_ipv4, ssh_client = init_ec2()
+    tika_server_url = 'http://{}:9998/'.format(ec2_ipv4)
+
     for dce_data in dce_data_list:
         annonce_id, org_acronym, filename_reglement, filename_complement, filename_avis, filename_dce = dce_data
-        index_dce(annonce_id, org_acronym, filename_reglement, filename_complement, filename_avis, filename_dce, connection, cursor)
+        index_dce(annonce_id, org_acronym, filename_reglement, filename_complement, filename_avis, filename_dce, connection, cursor, tika_server_url)
+    
+    terminate_ec2(ec2_client, instance_id, ssh_client)
 
     cursor.close()
     connection.close()
 
 
-def index_dce(annonce_id, org_acronym, filename_reglement, filename_complement, filename_avis, filename_dce, connection, cursor):
+def index_dce(annonce_id, org_acronym, filename_reglement, filename_complement, filename_avis, filename_dce, connection, cursor, tika_server_url):
     """index_dce(): Extract the content of one DCE and give it to ElasticSearch
     """
 
@@ -64,7 +71,7 @@ def index_dce(annonce_id, org_acronym, filename_reglement, filename_complement, 
         if CONFIG_ENV['env'] != 'production':
             print('Extracting content of {}...'.format(internal_filepath))
 
-        content, embedded_resource_paths = extract_file(internal_filepath)
+        content, embedded_resource_paths = extract_file(internal_filepath, tika_server_url)
 
         psql_request_template = """
             UPDATE dce
@@ -117,13 +124,13 @@ def feed_elastisearch(annonce_id, org_acronym, content):
     assert response.status_code == 201, (response.status_code, response.text)
 
 
-def extract_file(file_path):
-    url = urllib.parse.urljoin(CONFIG_TIKA['tika_server_url'], '/rmeta/text')
+def extract_file(file_path, tika_server_url):
+    url = urllib.parse.urljoin(tika_server_url, '/rmeta/text')
     headers = {
-        'fileUrl': 'file://{}'.format(file_path),
         'Accept': 'application/json',
     }
-    response = requests.put(url, headers=headers)
+    with open(file_path, 'rb') as file_object:
+        response = requests.put(url, headers=headers, data=file_object)
     assert response.status_code == 200, (response.status_code, response.text)
 
     tika_result = json.loads(response.content)  # better than r.text that takes hours to compute
@@ -170,3 +177,86 @@ def is_unwanted_type(filename):
     }:
         return False
     return True
+
+
+def init_ec2():
+    ec2_client = boto3.client(
+        'ec2',
+        aws_access_key_id=CONFIG_AWS_EC2['aws_access_key_id'],
+        aws_secret_access_key=CONFIG_AWS_EC2['aws_secret_access_key'],
+        region_name=CONFIG_AWS_EC2['region_name'],
+    )
+
+    if CONFIG_ENV['env'] != 'production':
+        print('Launching EC2 instance...')
+
+    response = ec2_client.run_instances(
+        BlockDeviceMappings=[
+            {
+                'DeviceName': '/dev/xvda',
+                'Ebs': {
+                    'DeleteOnTermination': True,
+                    'VolumeSize': 8,
+                    'VolumeType': 'gp2',
+                },
+            },
+        ],
+        ImageId='ami-5ce55321',
+        InstanceType='t2.large',
+        KeyName=CONFIG_AWS_EC2['key_name'],
+        MaxCount=1,
+        MinCount=1,
+        SecurityGroupIds=[CONFIG_AWS_EC2['security_group']],
+        #DryRun=True,
+    )
+
+    instance_id = response['Instances'][0]['InstanceId']
+
+    waiter = ec2_client.get_waiter('instance_status_ok')
+    waiter.wait(
+        InstanceIds=[instance_id]
+    )
+
+    response = ec2_client.describe_instances(
+        InstanceIds=[instance_id]
+    )
+    ec2_ipv4 = response['Reservations'][0]['Instances'][0]['PublicIpAddress']
+
+    if CONFIG_ENV['env'] != 'production':
+        print('Successfully launched instance {} with IPv4 {}'.format(instance_id, ec2_ipv4))
+
+    ssh_client = paramiko.SSHClient()
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh_key = paramiko.RSAKey.from_private_key_file(CONFIG_AWS_EC2['private_key'])
+    ssh_client.connect(
+        hostname=ec2_ipv4,
+        username="ec2-user",
+        pkey=ssh_key
+    )
+
+    ssh_channel = ssh_client.get_transport().open_session()
+    ssh_channel.exec_command('wget http://apache.crihan.fr/dist/tika/tika-server-1.17.jar')
+    assert ssh_channel.recv_exit_status() == 0
+
+    ssh_channel = ssh_client.get_transport().open_session()
+    ssh_channel.exec_command('sudo yum -y install java')
+    assert ssh_channel.recv_exit_status() == 0
+
+    ssh_channel = ssh_client.get_transport().open_session()
+    ssh_channel.exec_command('java -jar tika-server-1.17.jar --host=*')
+
+    if CONFIG_ENV['env'] != 'production':
+        print('Launched Tika server')
+
+    return ec2_client, instance_id, ec2_ipv4, ssh_client
+
+
+def terminate_ec2(ec2_client, instance_id, ssh_client):
+    ssh_client.close()
+
+    ec2_client.terminate_instances(
+        InstanceIds=[instance_id],
+    )
+
+    if CONFIG_ENV['env'] != 'production':
+        print('Terminated EC2 instance')
--- scripts/ec2.sh
+++
@@ -1,3 +0,0 @@
-wget http://apache.crihan.fr/dist/tika/tika-server-1.17.jar
-yum -y install java
-java -jar tika-server-1.17.jar --host=*
