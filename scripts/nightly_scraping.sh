3ec385dd4c6ef35160871745d1b34fca839905b7
Michel Blancard <github@michelblancard.fr>
2023-04-06 21:49:21 +0200

    feat: add script to scrap nightly

---
+++ scripts/nightly_scraping.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -o xtrace
+set -o errexit
+
+PYTHON_PATH=/home/debian/.local/share/virtualenvs/place/bin/python
+SCRAPER_PLACE_PATH=/srv/scraper-place/scraper_place
+$PYTHON_PATH $SCRAPER_PLACE_PATH/fetch.py
+$PYTHON_PATH $SCRAPER_PLACE_PATH/glacier.py
+sudo systemctl stop betterplace.service
+sudo systemctl stop elasticsearch.service
+sudo systemctl start tika.service
+sleep 60
+$PYTHON_PATH $SCRAPER_PLACE_PATH/extraction.py
+sudo systemctl stop tika.service
+sudo systemctl start elasticsearch.service
+sleep 60
+$PYTHON_PATH $SCRAPER_PLACE_PATH/indexation.py
+sudo systemctl start betterplace.service
